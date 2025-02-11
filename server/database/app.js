ec4ff329b9a76a7af7ab225100fa3ca17e97bfe0
Sayo Law <shayolaw@gmail.com>
2025-02-11 14:25:47 -0500

    Added all features

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,38 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+    try{
+        const documents = await Dealerships.find();
+        res.json(documents)
+    }
+    catch(error){
+        res.status(500).json({error: "Error fetching documents"})
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+try{
+    documents = await Dealerships.find({state: req.params.state}) 
+    res.send(documents)
+}
+catch{
+    res.status(500).json({error: "Error fetching document"})
+}
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+        try{
+            documents = await Dealerships.find({id: req.params.id}) 
+            res.send(documents)
+        }
+        catch{
+            res.status(500).json({error: "Error fetching document"})
+        }
+  
 });
 
 //Express route to insert review
