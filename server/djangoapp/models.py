a24bb24e2c10e5329654321cea076e70ac28596f
Sayo Law <shayolaw@gmail.com>
2025-02-11 22:00:40 -0500

    fixed lint problems

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,50 +1,39 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
 class CarMake(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    """Model representing a car manufacturer."""
+    name = models.CharField(max_length=100, verbose_name="Car Make Name")
+    description = models.TextField(verbose_name="Description")
 
     def __str__(self):
         return self.name
 
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    choices = [
+    """Model representing a car model under a car make."""
+    
+    # Choices for car type
+    CAR_TYPES = [
         ('SEDAN', 'Sedan'),
-        ('SUV','SUV'),
-        ('WAGON', 'Wagon')
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
     ]
-    type = models.CharField(max_length=10, choices = choices, default = 'SUV')
-    year = models.IntegerField(default=2023,
-    validators = [
-        MaxValueValidator(2023),
-        MinValueValidator(2015)
-    ])
+
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE, related_name="models"
+    )
+    name = models.CharField(max_length=100, verbose_name="Car Model Name")
+    type = models.CharField(
+        max_length=10, choices=CAR_TYPES, default='SUV', verbose_name="Car Type"
+    )
+    year = models.IntegerField(
+        default=2023,
+        validators=[MinValueValidator(2015), MaxValueValidator(2023)],
+        verbose_name="Manufacturing Year",
+    )
 
     def __str__(self):
-        return self.name
-
+        return f"{self.car_make.name} {self.name} ({self.year})"
--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,38 +1,53 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
+    """Populate database with CarMake and CarModel instances."""
+
+    # Car make data
     car_make_data = [
-        {"name":"NISSAN", "description":"Great cars. Japanese technology"},
-        {"name":"Mercedes", "description":"Great cars. German technology"},
-        {"name":"Audi", "description":"Great cars. German technology"},
-        {"name":"Kia", "description":"Great cars. Korean technology"},
-        {"name":"Toyota", "description":"Great cars. Japanese technology"},
+        {"name": "NISSAN", "description": "Great cars. Japanese technology"},
+        {"name": "Mercedes", "description": "Great cars. German technology"},
+        {"name": "Audi", "description": "Great cars. German technology"},
+        {"name": "Kia", "description": "Great cars. Korean technology"},
+        {"name": "Toyota", "description": "Great cars. Japanese technology"},
     ]
 
-    car_make_instances = []
-    for data in car_make_data:
-            car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+    # Create CarMake instances using bulk_create
+    car_makes = CarMake.objects.bulk_create([
+        CarMake(name=data["name"], description=data["description"])
+        for data in car_make_data
+    ])
 
+    # Map car make names to instances for easy lookup
+    car_make_lookup = {car_make.name: car_make for car_make in car_makes}
 
-    # Create CarModel instances with the corresponding CarMake instances
+    # Car model data
     car_model_data = [
-      {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
-      {"name":"Qashqai", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
-      {"name":"XTRAIL", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
-      {"name":"A-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
-      {"name":"C-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
-      {"name":"E-Class", "type":"SUV", "year": 2023, "car_make":car_make_instances[1]},
-      {"name":"A4", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
-      {"name":"A5", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
-      {"name":"A6", "type":"SUV", "year": 2023, "car_make":car_make_instances[2]},
-      {"name":"Sorrento", "type":"SUV", "year": 2023, "car_make":car_make_instances[3]},
-      {"name":"Carnival", "type":"SUV", "year": 2023, "car_make":car_make_instances[3]},
-      {"name":"Cerato", "type":"Sedan", "year": 2023, "car_make":car_make_instances[3]},
-      {"name":"Corolla", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4]},
-      {"name":"Camry", "type":"Sedan", "year": 2023, "car_make":car_make_instances[4]},
-      {"name":"Kluger", "type":"SUV", "year": 2023, "car_make":car_make_instances[4]},
-        # Add more CarModel instances as needed
+        {"name": "Pathfinder", "type": "SUV", "year": 2023, "car_make": "NISSAN"},
+        {"name": "Qashqai", "type": "SUV", "year": 2023, "car_make": "NISSAN"},
+        {"name": "XTRAIL", "type": "SUV", "year": 2023, "car_make": "NISSAN"},
+        {"name": "A-Class", "type": "SUV", "year": 2023, "car_make": "Mercedes"},
+        {"name": "C-Class", "type": "SUV", "year": 2023, "car_make": "Mercedes"},
+        {"name": "E-Class", "type": "SUV", "year": 2023, "car_make": "Mercedes"},
+        {"name": "A4", "type": "SUV", "year": 2023, "car_make": "Audi"},
+        {"name": "A5", "type": "SUV", "year": 2023, "car_make": "Audi"},
+        {"name": "A6", "type": "SUV", "year": 2023, "car_make": "Audi"},
+        {"name": "Sorrento", "type": "SUV", "year": 2023, "car_make": "Kia"},
+        {"name": "Carnival", "type": "SUV", "year": 2023, "car_make": "Kia"},
+        {"name": "Cerato", "type": "Sedan", "year": 2023, "car_make": "Kia"},
+        {"name": "Corolla", "type": "Sedan", "year": 2023, "car_make": "Toyota"},
+        {"name": "Camry", "type": "Sedan", "year": 2023, "car_make": "Toyota"},
+        {"name": "Kluger", "type": "SUV", "year": 2023, "car_make": "Toyota"},
     ]
 
-    for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+    # Create CarModel instances using bulk_create
+    CarModel.objects.bulk_create([
+        CarModel(
+            name=data["name"],
+            car_make=car_make_lookup[data["car_make"]],
+            type=data["type"],
+            year=data["year"]
+        )
+        for data in car_model_data
+    ])
